306d638b9f7cd20d9981f09a0ddcda01b62c2f2b
yaluikang <lukankin.aleksandr@mail.ru>
2020-07-03 00:40:59 +0300

    views 1.3

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -29,7 +29,7 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-        'js/libs/jquery-3.3.1.min.js',
+        /*'js/libs/jquery-3.3.1.min.js',*/
         'js/libs/popper.min.js',
         'js/libs/bootstrap.min.js',
         'js/navigation.js',
