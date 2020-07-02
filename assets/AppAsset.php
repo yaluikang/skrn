ede33941f25f10dea95d9514165e6178a188dd7a
yaluikang <lukankin.aleksandr@mail.ru>
2020-07-03 00:42:13 +0300

    views 1.4

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -25,15 +25,15 @@ class AppAsset extends AssetBundle
         'css/landing-page-styles.css'
     ];
     public $js = [
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-        /*'js/libs/jquery-3.3.1.min.js',*/
+        'js/libs/jquery-3.3.1.min.js',
         'js/libs/popper.min.js',
         'js/libs/bootstrap.min.js',
         'js/navigation.js',
         'js/jquery.flexslider-min.js',
         'js/script.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset'
     ];
 }
