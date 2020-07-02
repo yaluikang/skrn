f06d4abc94295062536580604b894c1eba94af60
yaluikang <lukankin.aleksandr@mail.ru>
2020-07-03 00:30:17 +0300

    views 1.1

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/bootstrap.css',
+        'css/landing-page-styles.css'
     ];
     public $js = [
     ];
