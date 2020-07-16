122f4cac4e934cf47c1c623a10be7dbe4a2b0e4c
yaluikang <lukankin.aleksandr@mail.ru>
2020-07-16 23:31:22 +0300

    Exper. models 1.2

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=skrn',
     'username' => 'lukash',
     'password' => '76Agiheg',
     'charset' => 'utf8',
