5d17128ccb4cb31664ddd7cdc2c87814629d4376
yaluikang <lukankin.aleksandr@mail.ru>
2020-07-16 23:29:52 +0300

    Exper. models 1.1

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'lukash',
+    'password' => '76Agiheg',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
