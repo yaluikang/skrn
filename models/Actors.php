84588483b4ac3840aae27ee7a39e6c5600e30d5e
yaluikang <lukankin.aleksandr@mail.ru>
2020-08-08 10:39:23 +0300

    Actors 1.2

--- models/Actors.php
+++ models/Actors.php
@@ -8,6 +8,11 @@ use yii\db\ActiveRecord;
 
 class Actors extends ActiveRecord
 {
+    public static function tableName ()
+    {
+        return 'Actors';
+    }
+
     public function getMovies()
     {
         return $this->hasMany(Movies::class, ['Actor_id' => 'Id'])->viaTable('Movie_has_actors', ['Id' => 'Movie_id']);
