e69ecdd9600a84f87fce9831feaeebc71d862329
Yaluikang <lukankin.aleksandr@mail.ru>
2020-09-30 01:14:55 +0300

    Genres 2.9

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -97,7 +97,7 @@ class SiteController extends Controller
 
         //$movie = Genres::findOne(2);
 
-        $movies = Genres::find()->where(['Name' => 'боевик'])->all();
+        $movies = Genres::find()->where(['Name' => 'драма'])->one();
         //return (($movies->movies)[0])->Name;
 
         echo json_encode( (($movies->movies)[0])->Name, JSON_UNESCAPED_UNICODE );
